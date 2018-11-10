6216a2ae868921322892c6e39ba689bf53319176
Johnny Packard <37680772+johnnypackard@users.noreply.github.com>
2018-11-10 16:23:40 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ class App extends Component {
 
     componentWillMount() {
         firebase.initializeApp({
-            apiKey: 'AIzaSyCWb0lhHrWcMZo0WmtOFHavSxOjGybF5wQ',
-            authDomain: 'auth-40911.firebaseapp.com',
-            databaseURL: 'https://auth-40911.firebaseio.com',
-            projectId: 'auth-40911',
-            storageBucket: 'auth-40911.appspot.com',
-            messagingSenderId: '387513575347'
+            // empty strings only to prevent security leaks
+            // add your own firebase info here
+            apiKey: '',
+            authDomain: '',
+            databaseURL: '',
+            projectId: '',
+            storageBucket: '',
+            messagingSenderId: ''
           });
 
         firebase.auth().onAuthStateChanged((user) => {
