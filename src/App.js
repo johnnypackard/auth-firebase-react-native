2963be7eac8745b886c91142c71f93ac4a4ed5ee
Johnny Packard <packard81@me.com>
2018-11-10 13:38:50 -0600

    adding firebase to project and initialize on component will mount

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyCWb0lhHrWcMZo0WmtOFHavSxOjGybF5wQ',
+            authDomain: 'auth-40911.firebaseapp.com',
+            databaseURL: 'https://auth-40911.firebaseio.com',
+            projectId: 'auth-40911',
+            storageBucket: 'auth-40911.appspot.com',
+            messagingSenderId: '387513575347'
+          });
+    }
+    
     render() {
         return (
             <View>
