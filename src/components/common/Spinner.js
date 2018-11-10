eba2a5c4950147a210a30c7dfa2ab437c1f0adf0
Johnny Packard <packard81@me.com>
2018-11-10 14:49:00 -0600

    adding activityIndicator to spinner.js

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 
 const Spinner = () => {
     return(
         <View>
-
+            <ActivityIndicator />
         </View>
     );
 }
